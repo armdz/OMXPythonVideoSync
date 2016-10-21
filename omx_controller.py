fc17896e64d427404cadb1e7089ce7f3d91d532d
lolo <lolo@lolos-MacBook-Pro.local>
2016-10-21 02:50:44 -0300

    agregado ready,seek,pause

--- omx_controller.py
+++ omx_controller.py
@@ -53,7 +53,7 @@ class OMXController():
 			print "* OMX: READY, ESPERANDO PLAY *"
 	def rewind(self):
 		self.seek(0)
-		time.sleep(.5)
+		time.sleep(.1)
 		self.pause()
 	def play(self):
 		print "* RECIBO PLAY *"
